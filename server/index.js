9f78c7e0fd7863759f1643e58fcb5d79fe0352ec
NateWilemon <nathanielwilemon@gmail.com>
2026-05-12 21:05:45 -0700

    readded loadProceudres to init, still no load DB as we set that up on railway

--- server/index.js
+++ server/index.js
@@ -5,14 +5,8 @@ const cors = require('cors');
 app.use(cors());
 const dotenv = require('dotenv');
 dotenv.config();
+const fs = require('fs');
 const mysql = require('mysql2');
-
-console.log('DB config:', {
-    host: process.env.MYSQLHOST,
-    port: process.env.MYSQLPORT,
-    user: process.env.MYSQLUSER,
-    database: process.env.MYSQLDATABASE
-})
 
 const con = mysql.createConnection({
     host: process.env.MYSQLHOST,
@@ -25,7 +19,25 @@ const con = mysql.createConnection({
 con.connect(function(err) {
     if (err) throw err;
     console.log("Connected to DB.");
+    loadProcedures();
     require('./users')(app, con);
     require('./matchmaking')(app, con);
     app.listen(3001, () => console.log('Port is 3001'));
 });
+
+function loadProcedures() {
+    let sql = fs.readFileSync('procedures.sql', 'utf8');
+    sql = sql.replace(/--[^\n]*/g, '');
+    let procedures = sql
+        .replace(/DELIMITER \$\$/g, '')
+        .replace(/DELIMITER ;/g, '')
+        .split('$$')
+        .map(p => p.trim())
+        .filter(p => p.toLowerCase().includes('create procedure'));
+    procedures.forEach(proc => {
+        con.query(proc, err => {
+            if (err && err.code !== 'ER_SP_ALREADY_EXISTS') throw err;
+        });
+    });
+    console.log("Procedures loaded");
+}
