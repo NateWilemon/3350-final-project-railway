b3f2d32799adeb7703df31e79e5d93898378d21a
NateWilemon <nathanielwilemon@gmail.com>
2026-05-12 19:58:42 -0700

    changed sql connection to match railway env variables

--- server/index.js
+++ server/index.js
@@ -8,11 +8,11 @@ dotenv.config();
 const mysql = require('mysql2');
 
 const con = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE
 });
 
 con.connect(function(err) {
